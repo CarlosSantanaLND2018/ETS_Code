ecc4fca2da014658cee4999a3b747631aafb44b0
AlumGS <AlumGS@PCProfesor>
2019-05-15 17:53:32 +0200

    jaja se programar C:

--- src/aplicacion/Aplicacion.java
+++ src/aplicacion/Aplicacion.java
@@ -6,7 +6,13 @@ public class Aplicacion {
 
     
     public static void main(String[] args) {
-       /** prueba**/ 
+       int a = 5;
+       int b = 6;
+       int suma = 0 ;
+       
+       suma = a + b;
+       
+       System.out.println(suma);
        
     }
     
